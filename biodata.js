796cf169d8c4623239cc547728664248f1417057
MuhamadEzraHaikal <163391146+MuhamadEzraHaikal@users.noreply.github.com>
2024-09-24 09:31:29 +0700

    Add files via upload

---
+++ biodata.js
@@ -0,0 +1,14 @@
+const nama = document.querySelector('#name');
+nama.textContent = 'Ezra Haikal';
+
+const ttl = document.querySelector('#TTL');
+ttl.textContent = 'Sukabumi, 06 Januari 2005';
+
+const Alamat = document.querySelector('#Alamat');
+Alamat.textContent = 'Ciseureuh gg Annur Rt 05 Rw 05 ';
+
+const Hobi = document.querySelector('#Hobi');
+Hobi.textContent = 'Melihat sesuatu yang indah ';
+
+const Agama = document.querySelector('#Agama');
+Agama.textContent = 'Islam ';
